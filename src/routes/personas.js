3664e06f5a209153573beba7bd91b21600b90fab
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-25 10:31:30 -0600

    modified:   src/routes/personas.js

--- src/routes/personas.js
+++ src/routes/personas.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('../db');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 
 
@@ -24,16 +25,20 @@ function ensureToken(req,res,next) {
 
 // LEER TODA LA TABLA
 router.get('/',(req,res)=>{
-    const sql = `CALL PRC_PERSONAS('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 4, '')`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        if (results.length>0) {
-            res.json(results[0]);
-        }else{
-            res.send('No se pudieron Obtener los datos')
+    try {
+      const sql = `CALL PRC_PERSONAS('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 4, '')`;
+      mysql.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+          res.json(results[0]);
+        } else {
+          res.send("No se pudieron Obtener los datos");
         }
-    })
-    console.log('Datos Leidos Correctamente'); //confirmacion en Consola posteriormente se debe eliminar en produccion
+      });
+      console.log("Datos Leidos Correctamente"); //confirmacion en Consola posteriormente se debe eliminar en produccion
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 
@@ -54,36 +59,39 @@ router.get('/:cod',(req,res)=>{
 
 // INSERTAR 
 router.post('/insertar',ensureToken,(req,res)=>{
-    jwt.verify(req.token,clavesecreta,(err,data)=>{
-        if(err){
-            res.sendStatus(403);
-        }else{
-    const objpersonas = {
-        NOM_PERSONA: req.body.NOM_PERSONA,
-        SEX_PERSONA: req.body.SEX_PERSONA,
-        EDA_PERSONAL: req.body.EDA_PERSONAL,
-        TIP_PERSONA: req.body.TIP_PERSONA,
-        Num_Identidad: req.body.Num_Identidad,
-        IND_CIVIL: req.body.IND_CIVIL,
-        IND_PERSONA: req.body.IND_PERSONA,
-        TELEFONO: req.body.TELEFONO,
-        TIP_TELEFONO: req.body.TIP_TELEFONO,
-        CORREO: req.body.CORREO,
-        PREGUNTA: req.body.PREGUNTA,
-        RESPUESTA: req.body.RESPUESTA,
-        USUARIO: req.body.USUARIO,
-        PASSWORD: req.body.PASSWORD,
-        ROL: req.body.ROL
-
+    try {
+      jwt.verify(req.token, process.env.JWT, (err, data) => {
+        if (err) {
+          res.sendStatus(403);
+        } else {
+          const objpersonas = {
+            NOM_PERSONA: req.body.NOM_PERSONA,
+            SEX_PERSONA: req.body.SEX_PERSONA,
+            EDA_PERSONAL: req.body.EDA_PERSONAL,
+            TIP_PERSONA: req.body.TIP_PERSONA,
+            Num_Identidad: req.body.Num_Identidad,
+            IND_CIVIL: req.body.IND_CIVIL,
+            IND_PERSONA: req.body.IND_PERSONA,
+            TELEFONO: req.body.TELEFONO,
+            TIP_TELEFONO: req.body.TIP_TELEFONO,
+            CORREO: req.body.CORREO,
+            PREGUNTA: req.body.PREGUNTA,
+            RESPUESTA: req.body.RESPUESTA,
+            USUARIO: req.body.USUARIO,
+            PASSWORD: req.body.PASSWORD,
+            ROL: req.body.ROL,
+          };
+          const sql = `CALL PRC_PERSONAS('${objpersonas.NOM_PERSONA}', '${objpersonas.SEX_PERSONA}',${objpersonas.EDA_PERSONAL} , '${objpersonas.TIP_PERSONA}', '${objpersonas.Num_Identidad}','${objpersonas.IND_CIVIL}','${objpersonas.IND_PERSONA}',${objpersonas.TELEFONO},'${objpersonas.TIP_TELEFONO}','${objpersonas.CORREO}','${objpersonas.PREGUNTA}','${objpersonas.RESPUESTA}','${objpersonas.USUARIO}','${objpersonas.PASSWORD}',${objpersonas.ROL},1, '?')`;
+          mysql.query(sql, (error, results) => {
+            if (error) throw error;
+            res.send("Datos insertados");
+          });
+        }
+      });
+      console.log("Datos insertados Correctamente"); //confirmacion en Consola posteriormente se debe eliminar en produccion
+    } catch (error) {
+      res.send(error);
     }
-    const sql = `CALL PRC_PERSONAS('${objpersonas.NOM_PERSONA}', '${objpersonas.SEX_PERSONA}',${objpersonas.EDA_PERSONAL} , '${objpersonas.TIP_PERSONA}', '${objpersonas.Num_Identidad}','${objpersonas.IND_CIVIL}','${objpersonas.IND_PERSONA}',${objpersonas.TELEFONO},'${objpersonas.TIP_TELEFONO}','${objpersonas.CORREO}','${objpersonas.PREGUNTA}','${objpersonas.RESPUESTA}','${objpersonas.USUARIO}','${objpersonas.PASSWORD}',${objpersonas.ROL},1, '?')`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        res.send("Datos insertados")
-    })
-}
-})
-    console.log('Datos insertados Correctamente'); //confirmacion en Consola posteriormente se debe eliminar en produccion
 });
 
 
