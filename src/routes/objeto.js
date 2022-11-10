6a969ed60b4bb587dae4cfb559f1404f7c425961
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-10 17:12:10 -0600

    modified:   src/routes/objeto.js

--- src/routes/objeto.js
+++ src/routes/objeto.js
@@ -19,91 +19,130 @@ function ensureToken(req,res,next) {
       res.sendStatus(403); //acceso prohibido
   }
  }
- const clavesecreta= 'ZAKESTHtw1243rtewgds08523765432379';
 // LEER TODA LA TABLA
-router.get('/objetos',ensureToken,(req,res)=>{
-    jwt.verify(req.token,clavesecreta,(err,data)=>{
-        if(err){
-            res.sendStatus(403);
-        }else{
-            const sql = `CALL PRC_OBJETOS('', '', '',  4, '')`;
-            mysql.query(sql,(error,results)=>{
-                if(error) throw error;
-                if (results.length>0) {
-                    res.json(results[0]);
-                }else{
-                    res.send('No se pudieron Obtener los datos')
-                }
-            });
-        }
-    })
-    console.log('Datos Leidos Correctamente');
+router.get("/objetos", ensureToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT, (err, data) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const sql = `CALL PRC_OBJETOS('', '', '',  4, '')`;
+        mysql.query(sql, (error, results) => {
+          if (error) throw error;
+          if (results.length > 0) {
+            res.json(results[0]);
+          } else {
+            res.send("No se pudieron Obtener los datos");
+          }
+        });
+      }
+    });
+    console.log("Datos Leidos Correctamente");
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 
 //BUSCAR POR ID
-router.get('/objetos/:cod',(req,res)=>{
-    const {cod} = req.params;
-    const sql = `CALL PRC_OBJETOS('', '', '', 5, ${cod})`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        if (results.length>0) {
+router.get("/objetos/:cod", ensureToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT, (err, data) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const { cod } = req.params;
+        const sql = `CALL PRC_OBJETOS('', '', '', 5, ${cod})`;
+        mysql.query(sql, (error, results) => {
+          if (error) throw error;
+          if (results.length > 0) {
             res.json(results[0]);
-        }else{
-            res.send('No se pudieron Obtener los datos')
-        }
-    })
-    console.log('Datos Leidos Correctamente');
+          } else {
+            res.send("No se pudieron Obtener los datos");
+          }
+        });
+        console.log("Datos Leidos Correctamente");
+      }
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // INSERTAR 
-router.post('/objetos/insertar',(req,res)=>{
-    const objobjetos = {
-      
-        OBJETO: req.body.OBJETO,
-        DES_OBJETO: req.body.DES_OBJETO,
-        TIP_OBJETO: req.body.TIP_OBJETO
-        
-    }
-    const sql = `CALL PRC_OBJETOS( '${objobjetos.OBJETO}','${objobjetos.DES_OBJETO}' , '${objobjetos.TIP_OBJETO}',  1, '?')`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        res.send("Datos insertados")
-    })
-    console.log('Datos insertados Correctamente');
+router.post("/objetos/insertar", ensureToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT, (err, data) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const objobjetos = {
+          OBJETO: req.body.OBJETO,
+          DES_OBJETO: req.body.DES_OBJETO,
+          TIP_OBJETO: req.body.TIP_OBJETO,
+        };
+        const sql = `CALL PRC_OBJETOS( '${objobjetos.OBJETO}','${objobjetos.DES_OBJETO}' , '${objobjetos.TIP_OBJETO}',  1, '?')`;
+        mysql.query(sql, (error, results) => {
+          if (error) throw error;
+          res.send("Datos insertados");
+        });
+        console.log("Datos insertados Correctamente");
+      }
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 
 // ACTUALIZAR
-router.put('/objetos/actualizar/:cod',(req,res)=>{
-    const {cod} = req.params;
-    const objobjetos = {
-        COD_OBJETO: req.body.COD_OBJETO,
-        OBJETO: req.body.OBJETO,
-        DES_OBJETO: req.body.DES_OBJETO,
-        TIP_OBJETO: req.body.TIP_OBJETO
-    }
-    const sql = `CALL PRC_OBJETOS('${objobjetos.OBJETO}','${objobjetos.DES_OBJETO}' , '${objobjetos.TIP_OBJETO}', 2, ${cod})`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        res.send("Datos Actualizados")
-        
-    })
-   
-    console.log('Datos Actualizados Correctamente');
+router.put("/objetos/actualizar/:cod", ensureToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT, (err, data) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const { cod } = req.params;
+        const objobjetos = {
+          COD_OBJETO: req.body.COD_OBJETO,
+          OBJETO: req.body.OBJETO,
+          DES_OBJETO: req.body.DES_OBJETO,
+          TIP_OBJETO: req.body.TIP_OBJETO,
+        };
+        const sql = `CALL PRC_OBJETOS('${objobjetos.OBJETO}','${objobjetos.DES_OBJETO}' , '${objobjetos.TIP_OBJETO}', 2, ${cod})`;
+        mysql.query(sql, (error, results) => {
+          if (error) throw error;
+          res.send("Datos Actualizados");
+        });
+
+        console.log("Datos Actualizados Correctamente");
+      }
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // ELIMINAR 
-router.delete('/objetoS/eliminar/:cod',(req,res)=>{
-    const {cod} = req.params;
-    const sql = `CALL PRC_OBJETOS('', '', '',  3, ${cod})`;
-    mysql.query(sql,(error,results)=>{
-        if(error) throw error;
-        res.send("Datos Eliminados")
-        
-    })
-   
-    console.log('Datos Eliminados Correctamente');
+router.delete("/objetoS/eliminar/:cod", ensureToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT, (err, data) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const { cod } = req.params;
+        const sql = `CALL PRC_OBJETOS('', '', '',  3, ${cod})`;
+        mysql.query(sql, (error, results) => {
+          if (error) throw error;
+          res.send("Datos Eliminados");
+        });
+
+        console.log("Datos Eliminados Correctamente");
+      }
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
