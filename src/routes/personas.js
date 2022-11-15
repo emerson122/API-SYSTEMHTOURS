fdb7ff19b0c5a645eb807ff97d30ca07e2a6d886
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-15 17:55:13 -0600

    modified:   src/routes/personas.js

--- src/routes/personas.js
+++ src/routes/personas.js
@@ -94,7 +94,7 @@ router.post("/usuarios", ensureToken,(req, res) => {
 });
 
 // traer usuarios
-router.post("/usuarios", ensureToken,(req, res) => {
+router.get("/list_usuarios", ensureToken,(req, res) => {
   try {
     jwt.verify(req.token, process.env.JWT, (err, data) => {
       if (err) {
